625d3997ddb1fea2efe8f68bc4132409aa230a8e
maratms <maratkagyu@gmail.com>
2026-07-24 17:09:38 +0200

    Install Pixi and add a test animation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,13 @@
+import PixiBackground from "../PixiBackground";
+import styles from "./Home.module.css";
+
 export default function Home() {
-  return <p>This is my test site. It works on my Raspberry PI 5! At home!</p>;
+  return (
+    <>
+      <PixiBackground />
+      <p className={styles.caption}>
+        This is my test site. It works on my Raspberry PI 5! At home!
+      </p>
+    </>
+  );
 }
